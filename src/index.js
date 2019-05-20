5b0e12d3bdeef5693f4ab510b8ec3c9150adce0b
liuhuan17 <liuhuan17@baidu.com>
2019-05-20 16:31:26 +0800

    添加HtmlWebpackPlugin生成HTML文件

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // import Vue from '../node_modules/vue/dist/vue.js'
 import Vue from 'vue';
 import app from './App.vue';
+
 new Vue({
-  render: h => h(app)
+    render: h => h(app)
 }).$mount('#app');
